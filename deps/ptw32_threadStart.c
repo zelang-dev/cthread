8939ed98a71e1508d2b7f63743542ba3af3efeec
Lawrence Stubbs <lstubbs@techno.express>
2024-06-24 11:09:32 -0400

    fix warning C4101: 'setjmp_rc': unreferenced

--- deps/ptw32_threadStart.c
+++ deps/ptw32_threadStart.c
@@ -19,17 +19,17 @@
  *      code distribution. The list can also be seen at the
  *      following World Wide Web location:
  *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
+ *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
  *      License as published by the Free Software Foundation; either
  *      version 2 of the License, or (at your option) any later version.
- * 
+ *
  *      This library is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *      Lesser General Public License for more details.
- * 
+ *
  *      You should have received a copy of the GNU Lesser General Public
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
@@ -141,6 +141,10 @@ ptw32_threadStart (void *vthreadParms)
   int setjmp_rc;
 #endif
 
+#if defined(PTW32_CLEANUP_C)
+(void)setjmp_rc;
+#endif
+
   ptw32_mcs_local_node_t stateLock;
   void * status = (void *) 0;
 
@@ -211,7 +215,7 @@ ptw32_threadStart (void *vthreadParms)
   __except (EXCEPTION_EXECUTE_HANDLER)
   {
       /*
-       * Some other exception occurred. 
+       * Some other exception occurred.
 	   * Abort the thread and mark it as canceled.
 	   * Do not terminate the application.
        */
